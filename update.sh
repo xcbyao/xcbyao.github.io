398a2427bd852238a8bb1a2771657bccebb1d6fd
xcbyao <xcbyao@gmail.com>
2021-04-18 19:32:04 +0800

    auto-update

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
-echo 'auto-update from https://www.zhihu.com/question/38962022'
+! on 2021.4.18 by xcbyao
+echo 'auto-update.sh from https://www.zhihu.com/question/38962022/answer/113416387'
+echo '----------------------------------------------------------------------------'
 hexo d -g && git add . && git commit -m 'auto-update' && git push origin hexo
